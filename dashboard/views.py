67927d12e1c544cad418d69024389a8844f9d424
Johann C. Rocholl <johann@rocholl.net>
2009-10-08 17:01:49 -0700

    Create missing backlinks for tags and suggestions.

--- dashboard/views.py
+++ dashboard/views.py
@@ -31,8 +31,10 @@ class ActionForm(forms.Form):
     """
     Actions to fix inconsistent tag references.
     """
-    create_missing_tags = forms.CharField(required=False)
-    delete_dangling_references = forms.CharField(required=False)
+    create_tags = forms.CharField(required=False)
+    delete_tag_suggestions = forms.CharField(required=False)
+    create_tag_suggestions = forms.CharField(required=False)
+    create_suggestion_tags = forms.CharField(required=False)
     adjust_tag_counts = forms.CharField(required=False)
 
 
@@ -43,40 +45,63 @@ def index(request):
     if suggestion_form.is_valid():
         return submit_suggestion(request, suggestion_form)
 
-    # Check consistency of suggestions and tags.
+    # Get all suggestions.
     suggestion_list = Suggestion.all().fetch(1000) # TODO: fetch unlimited
-    suggestion_names = [s.key().name() for s in suggestion_list]
-    tag_list = Tag.all().fetch(1000) # TODO: fetch unlimited
-    tag_names = [tag.key().name() for tag in tag_list]
+    suggestion_dict = {}
+    for suggestion in suggestion_list:
+        suggestion_dict[suggestion.key().name()] = suggestion
 
-    # Missing tags.
-    missing_tag_list = []
+    # Get all tags.
+    tag_list = Tag.all().fetch(1000) # TODO: fetch unlimited
+    tag_dict = {}
+    for tag in tag_list:
+        tag_dict[tag.key().name()] = tag
+
+    # Missing tags or references to suggestions.
+    missing_tags = []
+    missing_tag_suggestions = []
     for suggestion in suggestion_list:
         for tag in suggestion.tags:
-            if tag not in tag_names:
-                missing_tag_list.append((tag, suggestion))
-    missing_tag_list.sort()
+            if tag not in tag_dict:
+                missing_tags.append((tag, suggestion))
+                continue
+            tag = tag_dict[tag]
+            if suggestion.key().name() not in tag.suggestions:
+                missing_tag_suggestions.append((tag, suggestion))
+    missing_tags.sort()
+    missing_tag_suggestions.sort(key=lambda pair: pair[0].key().name())
 
-    # Missing suggestions.
-    missing_suggestion_list = []
-    for tag in tag_list:
-        for suggestion in tag.suggestions:
-            if suggestion not in suggestion_names:
-                missing_suggestion_list.append((suggestion, tag))
-    missing_suggestion_list.sort()
-
-    # Incorrect tag usage counts.
-    incorrect_count_list = []
+    # Missing suggestions or references to tags.
+    incorrect_tag_counts = []
+    missing_suggestions = []
+    missing_suggestion_tags = []
     for tag in tag_list:
         if tag.count != len(tag.suggestions):
-            incorrect_count_list.append(tag)
-    incorrect_count_list.sort(key=lambda tag: unicode(tag))
+            incorrect_tag_counts.append(tag)
+        for suggestion in tag.suggestions:
+            if suggestion not in suggestion_dict:
+                missing_suggestions.append((suggestion, tag))
+                continue
+            suggestion = suggestion_dict[suggestion]
+            if tag.key().name() not in suggestion.tags:
+                missing_suggestion_tags.append((suggestion, tag))
+    missing_suggestions.sort()
+    missing_suggestion_tags.sort(key=lambda pair: pair[0].key().name())
+    incorrect_tag_counts.sort(key=lambda tag: unicode(tag))
 
     # Fix inconsistencies if admin clicked one of the buttons.
     action_form = ActionForm(request.POST or None)
     if action_form.is_valid():
-        if action_form.cleaned_data.get('create_missing_tags', False):
-            return create_missing_tags(request, missing_tag_list)
+        if action_form.cleaned_data['create_tags']:
+            return create_tags(request, missing_tags)
+        if action_form.cleaned_data['delete_tag_suggestions']:
+            return delete_tag_suggestions(request, missing_suggestions)
+        if action_form.cleaned_data['adjust_tag_counts']:
+            return adjust_tag_counts(request, incorrect_tag_counts)
+        if action_form.cleaned_data['create_tag_suggestions']:
+            return create_tag_suggestions(request, missing_tag_suggestions)
+        if action_form.cleaned_data['create_suggestion_tags']:
+            return create_suggestion_tags(request, missing_suggestion_tags)
 
     # Show newest suggestions.
     day = datetime.now() - timedelta(hours=24)
@@ -125,14 +150,13 @@ def submit_suggestion(request, suggestion_form):
     return HttpResponseRedirect(suggestion.get_absolute_url())
 
 
-def create_missing_tags(request, missing_tag_list):
-    logging.debug('create_missing_tags')
-    while missing_tag_list:
-        key_name, suggestion = missing_tag_list.pop(0)
+def create_tags(request, missing_tags):
+    while missing_tags:
+        key_name, suggestion = missing_tags.pop(0)
         created = suggestion.created
         suggestion_names = [suggestion.key().name()]
-        while missing_tag_list and missing_tag_list[0][0] == key_name:
-            key_name, suggestion = missing_tag_list.pop(0)
+        while missing_tags and missing_tags[0][0] == key_name:
+            key_name, suggestion = missing_tags.pop(0)
             suggestion_names.append(suggestion.key().name())
             if suggestion.created < created:
                 created = suggestion.created
@@ -140,6 +164,46 @@ def create_missing_tags(request, missing_tag_list):
                   count=len(suggestion_names),
                   suggestions=suggestion_names,
                   created=created)
-        logging.debug(repr(tag))
         tag.put()
+        logging.debug('created %s', repr(tag))
     return HttpResponseRedirect(request.path)
+
+
+def delete_tag_suggestions(request, missing_suggestions):
+    for suggestion_name, tag in missing_suggestions:
+        tag.suggestions.remove(suggestion_name)
+        tag.count = len(tag.suggestions)
+        save_tag(tag)
+    return HttpResponseRedirect(request.path)
+
+
+def create_tag_suggestions(request, missing_tag_suggestions):
+    for tag, suggestion in missing_tag_suggestions:
+        tag.suggestions.append(suggestion.key().name())
+        tag.count = len(tag.suggestions)
+        save_tag(tag)
+    return HttpResponseRedirect(request.path)
+
+
+def create_suggestion_tags(request, missing_suggestion_tags):
+    for suggestion, tag in missing_suggestion_tags:
+        suggestion.tags.append(tag.key().name())
+        logging.debug('updating %s', repr(suggestion))
+        suggestion.put()
+    return HttpResponseRedirect(request.path)
+
+
+def adjust_tag_counts(request, incorrect_tag_counts):
+    for tag in incorrect_tag_counts:
+        tag.count = len(tag.suggestions)
+        save_tag(tag)
+    return HttpResponseRedirect(request.path)
+
+
+def save_tag(tag):
+    if tag.count:
+        logging.debug('updating %s', repr(tag))
+        tag.put()
+    else:
+        logging.debug('deleting %s', repr(tag))
+        tag.delete()
