1aecc2ea137017efaf0d16c58c8bcacc2007d837
Johann C. Rocholl <johann@rocholl.net>
2009-10-11 12:24:18 -0700

    Must use admin email for sender address.

--- settings.py
+++ settings.py
@@ -25,11 +25,11 @@ COMBINE_MEDIA = {
 
 
 # Change your email settings
-DEFAULT_FROM_EMAIL = 'support@minderbot.com'
-SERVER_EMAIL = DEFAULT_FROM_EMAIL
 ADMINS = (
-    ('Johann C. Rocholl', 'johann@rocholl.net'),
+    ('Johann C. Rocholl', 'jcrocholl@gmail.com'),
     )
+DEFAULT_FROM_EMAIL = ADMINS[0][1]
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '%7317$0d_3qpj7s_h3_ts_p+u&p&&#puc(&2p&lm=!$(uai@3f'
