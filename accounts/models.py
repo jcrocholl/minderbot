0ff34c106ac97bcbb914bd5510a06e2e1c7b11f2
Johann C. Rocholl <johann@rocholl.net>
2009-10-11 01:11:08 -0700

    Show first part of email instead of username.

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,10 @@ from ragendja.auth.models import EmailUser
 
 
 class User(EmailUser):
-    pass
+
+    def __unicode__(self):
+        email = self.email
+        at = email.find('@')
+        if at > 0:
+            email = email[:at]
+        return email
