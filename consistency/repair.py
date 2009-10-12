e16f46e5acf71c5111870d555d9b040eaa990e93
Johann C. Rocholl <johann@rocholl.net>
2009-10-11 17:04:46 -0700

    Sorted the repair functions.

--- consistency/repair.py
+++ consistency/repair.py
@@ -6,6 +6,59 @@ from suggestions.models import Suggestion
 from tags.models import Tag
 
 
+def save_tag(tag):
+    if tag.count:
+        logging.debug('updating %s', repr(tag))
+        tag.put()
+    else:
+        logging.debug('deleting %s', repr(tag))
+        tag.delete()
+
+
+def suggestion_author(request, problems):
+    for text, suggestion in problems:
+        suggestion.author = request.user
+        logging.debug('updating %s', repr(suggestion))
+        suggestion.put()
+    return HttpResponseRedirect(request.path)
+
+
+def suggestion_missing(request, problems):
+    for text, suggestion_name, tag in problems:
+        tag.suggestions.remove(suggestion_name)
+        tag.count = len(tag.suggestions)
+        save_tag(tag)
+    return HttpResponseRedirect(request.path)
+
+
+def suggestion_tag(request, problems):
+    for text, suggestion, tag in problems:
+        suggestion.tags.append(tag.key().name())
+        logging.debug('updating %s', repr(suggestion))
+        suggestion.put()
+    return HttpResponseRedirect(request.path)
+
+
+def tag_count(request, problems):
+    for text, tag, count, length in problems:
+        tag.count = len(tag.suggestions)
+        save_tag(tag)
+    return HttpResponseRedirect(request.path)
+
+
+def tag_created(request, problems):
+    for text, tag, suggestion in problems:
+        tag.created = suggestion.created
+        save_tag(tag)
+    return HttpResponseRedirect(request.path)
+
+
+def tag_empty(request, problems):
+    for text, tag in problems:
+        save_tag(tag)
+    return HttpResponseRedirect(request.path)
+
+
 def tag_missing(request, problems):
     problems.sort(key=lambda triple: triple[1])
     while problems:
@@ -26,62 +79,9 @@ def tag_missing(request, problems):
     return HttpResponseRedirect(request.path)
 
 
-def suggestion_missing(request, problems):
-    for text, suggestion_name, tag in problems:
-        tag.suggestions.remove(suggestion_name)
-        tag.count = len(tag.suggestions)
-        save_tag(tag)
-    return HttpResponseRedirect(request.path)
-
-
-def delete_empty_tags(request, empty_tags):
-    for text, tag in empty_tags:
-        save_tag(tag)
-    return HttpResponseRedirect(request.path)
-
-
-def create_tag_suggestions(request, missing_tag_suggestions):
-    for text, tag, suggestion in missing_tag_suggestions:
+def tag_suggestions(request, problems):
+    for text, tag, suggestion in problems:
         tag.suggestions.append(suggestion.key().name())
         tag.count = len(tag.suggestions)
         save_tag(tag)
     return HttpResponseRedirect(request.path)
-
-
-def suggestion_tag(request, problems):
-    for text, suggestion, tag in problems:
-        suggestion.tags.append(tag.key().name())
-        logging.debug('updating %s', repr(suggestion))
-        suggestion.put()
-    return HttpResponseRedirect(request.path)
-
-
-def suggestion_author(request, problems):
-    for text, suggestion in problems:
-        suggestion.author = request.user
-        logging.debug('updating %s', repr(suggestion))
-        suggestion.put()
-    return HttpResponseRedirect(request.path)
-
-
-def tag_count(request, problems):
-    for text, tag, count, length in problems:
-        tag.count = len(tag.suggestions)
-        save_tag(tag)
-    return HttpResponseRedirect(request.path)
-
-
-def adjust_tag_created(request, incorrect_tag_created):
-    for text, tag, suggestion in incorrect_tag_created:
-        tag.created = suggestion.created
-        save_tag(tag)
-    return HttpResponseRedirect(request.path)
-
-
-def save_tag(tag):
-    if tag.count:
-        logging.debug('updating %s', repr(tag))
-        tag.put()
-    else:
-        logging.debug('deleting %s', repr(tag))
-        tag.delete()
