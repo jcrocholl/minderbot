65121157b3dbe0176701cee84a99d53aaaaca253
Johann C. Rocholl <johann@rocholl.net>
2009-10-14 00:13:33 -0700

    Removed debug logging.

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,11 +1,7 @@
-import logging
-
-
 def appname(request):
     parts = request.path.split('/')
     while parts and not parts[0]:
         parts.pop(0)
     if not parts:
         parts.append('welcome')
-    logging.debug('appname %s', parts[0])
     return {'appname': parts[0]}
